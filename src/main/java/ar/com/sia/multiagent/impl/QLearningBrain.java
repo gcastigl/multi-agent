af3b5f8b2b3dbb722f15797cbc1d0a7c50eee749
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2013-03-29 17:26:38 -0300

    dasdas

--- src/main/java/ar/com/sia/multiagent/impl/QLearningBrain.java
+++ src/main/java/ar/com/sia/multiagent/impl/QLearningBrain.java
@@ -1,8 +1,11 @@
 package ar.com.sia.multiagent.impl;
 
+import ar.com.sia.algorithm.qlearning.QAction;
 import ar.com.sia.algorithm.qlearning.QMatrix;
+import ar.com.sia.algorithm.qlearning.QState;
 import ar.com.sia.multiagent.base.Agent;
 import ar.com.sia.multiagent.base.Brain;
+import ar.com.sia.multiagent.impl.train.qlearning.state.AgentQState;
 
 public class QLearningBrain implements Brain {
 
@@ -22,4 +25,16 @@ public class QLearningBrain implements Brain {
 		
 	}
 
+	private void findBestAction() {
+		QState currentState = getCurrentState();
+		for (QAction action : movementMatrix.getActions()) {
+			float profit = movementMatrix.get(currentState, action);
+			
+		}
+	}
+	
+	private QState getCurrentState() {
+		float[] position = agent.getModel().getPosition();
+		return new AgentQState((int) position[0], (int) position[1]);
+	}
 }
